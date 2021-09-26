501aef5c0fab8aa444686b18befd237f03ecc0f2
TheVy <ntvy2k@gmail.com>
2021-09-26 16:57:58 +0700

    add nginx, link together, test

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
+// import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import axios from "axios";
+
+const my_get = () => {
+  const url = "api/";
+  axios
+    .get(url)
+    .then((data) => {
+      console.log("Data:", data);
+      alert("Test: OK");
+    })
+    .catch(() => console.log("Data: failed"));
+};
 
 const Home: NextPage = () => {
   return (
@@ -12,7 +25,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>Hello Docker Change 100</div>
+      <div>Hello Django NextJS Nginx</div>
+      <button type="button" onClick={my_get}>
+        Django...
+      </button>
+      <div>
+        <Link href="/about">
+          <a>About Us</a>
+        </Link>
+      </div>
     </div>
   );
 };
