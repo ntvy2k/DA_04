a7f177a3c2d2cf0b84e1d45313e04cf2d8087d6c
TheVy <ntvy2k@gmail.com>
2021-10-04 15:35:45 +0700

    todo register

---
+++ frontend/pages/register.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Register = () => {
+  return (
+    <>
+      <Head>
+        <title>Register</title>
+        <meta name="description" content="Register an account" />
+      </Head>
+      <div>Hi</div>
+    </>
+  );
+};
+
+export default Register;
