c42f88dc239f61c0738c8065c22bde31de2a7df0
TheVy <ntvy2k@gmail.com>
2021-10-09 15:59:03 +0700

    admin site

--- api_da4/admin.py
+++ api_da4/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Course, Chapter, Lesson, Content
 
-# Register your models here.
+
+# Xong viec chac la xoa luon
+admin.site.register(Course)
+admin.site.register(Chapter)
+admin.site.register(Lesson)
+admin.site.register(Content)
