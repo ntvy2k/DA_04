94ccfed54949c8c024749d3b3abce2a11f7869df
TheVy <ntvy2k@gmail.com>
2021-10-14 15:32:57 +0700

    nhin co ve duoc r day

---
+++ auth_da4/utils.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+
+OK = 0
+USERNAME_ALREADY = 1
+EMAIL_ALREADY = 2
+
+class ExistUser:
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+    
+
+    def is_email_exists(self):
+        return User.objects.filter(email=self.email).exists()
+    
+
+    def is_username_exists(self):
+        return User.objects.filter(username=self.username).exists()
+    
+
+    def _exists(self):
+        if self.is_username_exists():
+            return USERNAME_ALREADY
+        if self.is_email_exists():
+            return EMAIL_ALREADY
+        return OK
+
+
+    def is_exists(self):
+        return self._exists()
