f1817663d21d362eb4d65d0aebbdf18aa3f4ea75
unknown <trongphuc5667@gmail.com>
2021-10-25 21:39:50 +0700

    ad

--- docker_django/data/course_icons.py
+++ docker_django/data/course_icons.py
@@ -7,5 +7,8 @@ ICONS = [
     {"name":"PHP","nontation":"devicon-php-plain"}
     {"name":"C","nontation":"devicon-c-plain"}
     {"name":"C++","nontation":"devicon-cplusplus-plain"}
+    {"name":"C#","nontation":"devicon-csharp-plain"}
+    {"name":"React","nontation":"devicon-react-original"}
+    {"name":"TypeScript","nontation":"devicon-typescript-plain"}
     # Them icon tai day
 ]
