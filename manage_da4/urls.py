dfafed15f6ee47e22eb8380b0e46d127ddf50798
TheVy <ntvy2k@gmail.com>
2021-10-26 00:39:50 +0700

    api root

--- manage_da4/urls.py
+++ manage_da4/urls.py
@@ -13,10 +13,10 @@ from .viewsets import (
 
 from django.urls import path, include
 
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register(r"group", ManageCourseGroupViewSet, basename="group")
 router.register(r"topic", ManageCourseTopicViewSet, basename="topic")
 router.register(r"icon", ManageCourseIconViewSet, basename="icon")
@@ -24,7 +24,6 @@ router.register(r"course", ManageCourseViewSet, basename="course")
 router.register(r"chapter", ManageChapterViewSet, basename="chapter")
 router.register(r"lesson", ManageLessonViewSet, basename="lesson")
 router.register(r"content", ManageContentViewSet, basename="content")
-
 
 urlpatterns = [
     path("", include(router.urls)),
