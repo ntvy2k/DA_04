d3c4b6cd376ce2f3a65be1a00aafd9dfdfbd46dc
TheVy <ntvy2k@gmail.com>
2021-10-26 00:35:20 +0700

    , nontation

--- docker_django/data/course_icons.py
+++ docker_django/data/course_icons.py
@@ -1,14 +1,14 @@
 # {"name": "...", "nontation": "..."}
 ICONS = [
     {"name": "HTML", "nontation": "devicon-html5-plain"},
-    {"name":"CSS","nontation":"devicon-css3-plain"},
-    {"name":"Javascript","nontation":"devicon-javascript-plain"}
-    {"name":"Python","nontation":"devicon-python-plain"}
-    {"name":"PHP","nontation":"devicon-php-plain"}
-    {"name":"C","nontation":"devicon-c-plain"}
-    {"name":"C++","nontation":"devicon-cplusplus-plain"}
-    {"name":"C#","nontation":"devicon-csharp-plain"}
-    {"name":"React","nontation":"devicon-react-original"}
-    {"name":"TypeScript","nontation":"devicon-typescript-plain"}
+    {"name": "CSS", "nontation": "devicon-css3-plain"},
+    {"name": "Javascript", "nontation": "devicon-javascript-plain"},
+    {"name": "Python", "nontation": "devicon-python-plain"},
+    {"name": "PHP", "nontation": "devicon-php-plain"},
+    {"name": "C", "nontation": "devicon-c-plain"},
+    {"name": "C++", "nontation": "devicon-cplusplus-plain"},
+    {"name": "C#", "nontation": "devicon-csharp-plain"},
+    {"name": "React", "nontation": "devicon-react-original"},
+    {"name": "TypeScript", "nontation": "devicon-typescript-plain"},
     # Them icon tai day
 ]
